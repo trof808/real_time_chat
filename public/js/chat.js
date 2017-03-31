d10a7d89d5c0f23a5229efc428391ff85efaa49d
trof <reatrof@gmail.com>
2017-03-31 11:06:27 +0300

    retransmission to server about new message, changing room or name

---
+++ public/js/chat.js
@@ -0,0 +1,50 @@
+//new class Chat with socket parameter
+const Chat = (socket) => {
+  this.socket = socket;
+};
+
+//send chat message method
+Chat.prototype.sendMessage = (room, text) => {
+    let message = {
+        room: room,
+        test: text
+    };
+
+    this.socket.emit('message', message);
+};
+
+//changing room method
+Chat.prototype.changeRoom = (room) => {
+    socket.emit('join', {
+        newRoom: room
+    });
+};
+
+//handling user commands
+Chat.prototype.processCommand = (command) => {
+    //get message to array
+    let words = command.split(" ");
+    //get command word from array
+    let command = words[0].substring(1, words[0].length).toLowerCase();
+
+    switch (command) {
+        //if command is join call method changeRoom
+        case 'join':
+            words.shift();
+            let room = words.join(" ");
+            this.changeRoom(room);
+            break;
+        case 'nick':
+            //if command is nick emit current socket with nameAttempt event
+            words.shift();
+            let nickname = words.join(" ");
+            this.socket.emit('nameAttempt', nickname);
+            break;
+        default:
+            //if command is incorrect return error message
+            message = 'Unrecognized command message';
+            break;
+    }
+    return message;
+};
+
